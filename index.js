301c188fa7726995feb1f4b5957b0ce9db9efb5f
ashishkumarrsm <ashishkumar141199@gmail.com>
2024-08-08 18:27:32 +0530

    lol

--- index.js
+++ index.js
@@ -1,60 +1,16 @@
-//  const math = require('./math');
-// const { add } = require("./math");
+const express = require("express");
+const app = express();
 
-// // const http = require('http');
+const sendMessage=  (req, res) => {
+    res.send("Hello bro");}
+// get post put delete request
 
-// // const server= http.createServer((req,res)=>{
-// //     console.log(req.url)
-// // res.end("heloo world")
-// // })
-// // server.listen(4000,()=>{
-// //     console.log('server is running on port 4000')
-// // });
+app.get("/", sendMessage);
 
-// let isloggein = true;
+app.get("/abc", (req, res) => {
+  res.send("Hello bro you are in lolo");
+});
 
-// if (isloggein === true) {
-//   console.log("you are loggdin");
-// } else {
-//   console.log("you are note log in");
-// }
-
-// console.log(math.add(2, 4));
-
-// os module
-const { log } = require('console');
-const os = require('os');
-const user = os.userInfo();
-console.log(user.username);
-console.log(os.platform());
-console.log(os.arch());
-console.log(os.homedir());
-const path = require('path');
-
-console.log(__dirname);
-console.log(__filename);
-
-console.log(path.basename(__filename));
-
-
-// file module
-const fs = require('fs');
-
-
-// let message = "Ha  again in leran node js"
-// fs.writeFile("example.txt",message,(err)=>{
-//     console.log(err);
-    
-// })
-
-
-fs.readFile('example.txt',"utf-8",(err,data)=>{
-    if(err){
-        console.log("error");
-        
-    }
-    else{
-        console.log(data);
-        
-    }
+app.listen(3000, () => {
+  console.log("Server Start on port 3000");
 })
