273674ac876ca43a67842750d2e16fce6687f7d5
ashishkumarrsm <ashishkumar141199@gmail.com>
2024-08-08 19:03:10 +0530

    express js

--- index.js
+++ index.js
@@ -1,15 +1,75 @@
 const express = require("express");
 const app = express();
 
-const sendMessage=  (req, res) => {
-    res.send("Hello bro");}
+const sendMessage = (req, res) => {
+  res.send("Hello bro");
+}
 // get post put delete request
 
-app.get("/", sendMessage);
+// get 
 
+app.get("/", sendMessage);
 app.get("/abc", (req, res) => {
   res.send("Hello bro you are in lolo");
 });
+
+
+
+app.post("/", (req, res) => {
+  res.send("post request");
+});
+app.put("/", (req, res) => {
+  res.send("post request");
+});
+
+
+
+const user = [
+  {
+    "id": 1,
+    "name": "John Doe",
+    "age": 30
+  },
+  {
+    "id": 2,
+    "name": "Jane Doe",
+    "age": 25
+
+  },
+  {
+    "id": 3,
+    "name": "John Smith",
+    "age": 35
+  }
+]
+
+app.get("/user/:id/:name", (req, res) => {
+  const isLoggdin = true;
+  const id = req.params.id;
+  console.log(req.params);
+  if (isLoggdin) {
+    res.send(user)
+  } else {
+    res.send("you are not logged in");
+  }
+
+  res.send("Hello bro you are in lolo");
+
+});
+
+
+
+app.use((req, res, next) => {
+  res.status(404).send(" 404 Page not found");
+})
+
+
+app.delete("/", (req, res) => {
+  res.send("post request");
+});
+
+
+
 
 app.listen(3000, () => {
   console.log("Server Start on port 3000");
